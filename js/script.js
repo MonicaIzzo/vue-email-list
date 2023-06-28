0f0b073a8593296e3cba3ca234a4e0efb667e34c
MonicaIzzo <info@monicaizzo.net>
2023-06-28 06:44:53 +0200

    devo finire ancora

--- js/script.js
+++ js/script.js
@@ -1,91 +1,44 @@
 /*
-Attraverso l'apposita API di Boolean :puntare_in_basso::carnagione-2:
+Attraverso l'apposita API di Boolean
 https://flynn.boolean.careers/exercises/api/random/mail
 generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 Bonus
 Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 */
 
-// # verifiche JS
-console.log('JS OK');
-console.log('Vue OK, Vue');
-
 // # Axios
-const element = document.getElementById('root');
-let content = '';
-axios.get('https://ﬂynn.boolean.careers/exercises/api/random/mail')
-.then(result => {
-  console.log(result.data);
-  content = result.data.response;
-  element.innerText = content;
-}).catch((err) => {
-  element.innerText = err.message;
-})
-.then(() => {
-  console.log('Chiamata terminata')
-});
-
-element.innerText = content;
-/*
+const endpoint = 'https://ﬂynn.boolean.careers/exercises/api/random/mail';
 // # inizializzo Vue JS
     const app = Vue.createApp ({
         data() {
             return {
-              newMessage: '',
-              currentNumber: null,
-              user: data.user,
-              contacts: data.contacts
+              mail: null,
+              mailList: null,
             }
         },
-
         computed: {
-          currentContact() {
-            return this.contacts.find(contact => contact.id === this.currentNumber)
-          },
-
-          currentChat() {
-            return this.currentContact.massages;
-          },
-
         },
-
         methods: {
-          renderPicture(person) {
-            return `img/avatar${person.avatar}.jpg`;
-          },
-          setCurrentNumber(number) {
-            this.currentNumber = number;
-          },
+          //createListemail() {
 
-          addMessage(message, status) {
-            const newMessage = {
-              id: new Date().getTime(),
-              date: "new Date().toLocalString()",
-              message,
-              status
-            };
-
-            this.currentChat.message.push(newMessage);
-
-          },
-
-          sendMessage () {
-            if(!this.newMessage) return;
-           
-            this.addMessage(this.newMessage, "sent");
-            this.newMessage ="";
-            
-            setTimeout(() => {
-              this.addMessage("ok!", "received");
-              }, 1000 );
           }
         },
         created () {
-          this.currentNumber = this.contacts[0].id;
-        }
+          // # Axios
+          axios.get(endpoint)
+          .then(res => {
+          console.log(res.data.response);
+          this.mail = res.data;
+          //content = result.data.response;
+          //element.innerText = content;
+          }).catch((err) => {
+          body.innerText = err.message;
+          })
+          .then(() => {
+          console.log('Chiamata terminata')
         
+          });
+        }
     });
-
-    //La monto nell'elemento HTML "radice"
+    //Monto nell'elemento HTML "radice"
     app.mount('#root');
-    */
