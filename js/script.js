cdbd5a64d4a49b96a9514faeb0d392a8fd174efd
MonicaIzzo <info@monicaizzo.net>
2023-06-28 05:37:40 +0200

    add Axios

--- js/script.js
+++ js/script.js
@@ -1,41 +1,31 @@
 /*
-Descrizione
-Trovate la descrizione dell'esercizio nel brief in allegato. Fate riferimento alla registrazione dell'assegnazione per eventuali dubbi. In alternativa non esitate a contattare me al mattino o i tutor al pomeriggio, taggandoci sul canale GENERAL
-Milestones
-Cercate di lavorare in ordine di milestone: sono state pensate con ordine logico per agevolare il vostro lavoro. Per oggi ci aspettiamo il completamento della Milestone 1 che riguarda per lo più la grafica.
-Vi ricordo che Milestone 5 è opzionale e che se ne aveste voglia abbiamo ulteriori bonus su richiesta.
-
-
-- **1 MILESTONE**
-  - Preparo la parte statica in HTML e CSS
-  - Copio il file data (ho scelto di importare il file data dall'esterno per avere un file script più semplice da leggere)
-- **2 MILESTONE**
-  - Userò l'ID come logica in tutto lo script per una maggiore coerenza con la prassi comune
-- **3 MILESTONE**
-  ? - Aggiunta di un messaggio: in console visualizzo dinamicamente il valore del campo riempito, ma non riesco a generare il nuovo messaggio. Probalbilmente c'è ub errore nel methods in "addMESSAGE".
-  ? - Risposta dall’interlocutore: idem il problema sta in "addMESSAGE".
-  **4 MILESTONE**
-  ? - Ricerca utenti: Sicuramente si fa con "filter" devo andarmi a vedere l'esercizio precedente (To do List) - (che non ho finito) 
- **5 MILESTONE**
-  ? - Cancella messaggio: idem per sopra è una funzione che abbiamo visto in TO DO LIST.
-  
-**BONUS**
-Dei bonus che vorrei fare:
-? - FARE LA INTRO DELLA CHAT come ha fatto Marco L. e sicuramente devo modificare il methods(created) Non fargli settare al caricamento il primo contatto corrente.
-? - FARE IL RESPONSIVE questo è facile, devo cambiare le col in class nell'HTML secondo la convenzione di Bootstrap.
-? - Se scrivo il messaggio nel campo testo l'icona accanto deve cambiare. (sicuramente è una funzione FOCUS con il ref="" da inserire in HTML)
-? - Mentre l'interlocutore mi scrive sotto alla contact list deve apparirmi il testo (sta scrivendo). Questo forse saprei come farlo, sicuramente andrà aggiunto nella methods in "addMESSAGE" e in setTimeout. Nell'HTML devo aggiungere lo <span> con dati dinamici.
-? - ELIMINAZIONE DEL CONTATTO idem con l'esercizio fatto TO DO LIST (se riesco a finirlo, tornerò qui ad aggiungerlo).
+Attraverso l'apposita API di Boolean :puntare_in_basso::carnagione-2:
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 */
-
-
-// # Preparo la parte statica in HTML e CSS
-
 
 // # verifiche JS
 console.log('JS OK');
 console.log('Vue OK, Vue');
 
+// # Axios
+const element = document.getElementById('root');
+axios.get('https://ﬂynn.boolean.careers/exercises/api/random/mail')
+.then(result => {
+  console.log(result.data);
+  content = result.data.response;
+  element.innerText = content;
+}).catch((err) => {
+  element.innerText = err.message;
+})
+.then(() => {
+  console.log('Chiamata terminata')
+});
+
+element.innerText = content;
+/*
 // # inizializzo Vue JS
     const app = Vue.createApp ({
         data() {
@@ -97,3 +87,4 @@ console.log('Vue OK, Vue');
 
     //La monto nell'elemento HTML "radice"
     app.mount('#root');
+    */
